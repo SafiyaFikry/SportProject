2db61945e11e092c293c819c7aef9620997c5fc1
Safiya Fikry <safiyafikry@gmail.com>
2023-05-21 03:19:22 +0300

    3 cells is almost done

--- SportProject/Modules/Events/View/SecondCollectionViewCell.swift
+++ SportProject/Modules/Events/View/SecondCollectionViewCell.swift
@@ -9,4 +9,12 @@ import UIKit
 
 class SecondCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var team2Score: UILabel!
+    @IBOutlet weak var team2Name: UILabel!
+    @IBOutlet weak var team2Img: UIImageView!
+    @IBOutlet weak var team1Score: UILabel!
+    @IBOutlet weak var team1Name: UILabel!
+    @IBOutlet weak var team1Img: UIImageView!
 }
